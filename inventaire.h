8a6f19cff9de6e93052546e83840024e839ba2b7
Iyaiya2 <iyad.hadjour@hotmail.com>
2025-11-12 21:41:07 +0100

    update inventaire.h

--- inventaire.h
+++ inventaire.h
@@ -1,7 +1,33 @@
 #ifndef INVENTAIRE_H
 #define INVENTAIRE_H
 
-void afficher_inventaire();
-void utiliser_objet();
+#define MAX_INVENTAIRE 8
+
+typedef struct {
+    char nom[30];
+    int attaque_min;
+    int attaque_max;
+    int defense;
+    int conso_oxygene;
+    char special[50];
+    int quantite; // pour consommables
+} Objet;
+
+typedef struct {
+    Objet objets[MAX_INVENTAIRE];
+    int nb_objets;
+    int harpon_equipe; // -1 si aucun
+    int combi_equipe;  // -1 si aucune
+    int perles;
+} Inventaire;
+
+// Fonctions
+void initialiser_inventaire(Inventaire *inv);
+void afficher_recompense(Objet o);
+void ajouter_objet_inventaire(Inventaire *inv, Objet o);
+void afficher_inventaire(Inventaire *inv);
+void utiliser_objet(Inventaire *inv, int index);
+void equiper_harpon(Inventaire *inv, int index);
+void equiper_combi(Inventaire *inv, int index);
 
 #endif
